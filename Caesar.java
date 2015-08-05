3b58b898b6b45b188c1f1f026709a6c1edc26a63
Pead Nong <anuchit.prasertsang@gmail.com>
2015-08-05 23:24:37 +0700

    reduce duplicat code

--- Caesar.java
+++ Caesar.java
@@ -11,17 +11,7 @@ public class Caesar {
    public String getCipher() {
      byte[] byteText = this.text.getBytes();
       
-     if (this.shift == 2) {
-       byteText[0] = (byte)(byteText[0]+2);        
-       return new String(byteText);
-     }
-
-     if (this.shift == 3) {
-       byteText[0] = (byte)(byteText[0]+3);        
-       return new String(byteText);
-     }
-     
-     byteText[0] = (byte)(byteText[0]+1);        
+     byteText[0] = (byte)(byteText[0]+this.shift);        
      return new String(byteText);
 
    }
