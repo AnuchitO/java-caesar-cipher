4ee1189476b5668940efcb30bf8de9f2fa35732c
Pead Nong <anuchit.prasertsang@gmail.com>
2015-08-06 00:04:22 +0700

    refactor Test case reduce duplicate code

--- CaesarTest.java
+++ CaesarTest.java
@@ -2,23 +2,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CaesarTest {
-  @Test
-  public void charAShift1ShoudleBeB(){
-    Caesar caesar = new Caesar("A", 1);
-    assertEquals("B", caesar.getCipher());
-  }
-
-
-  @Test
-  public void charAShift2ShouldBeC() {
-    Caesar caesar = new Caesar("A", 2);
-    assertEquals("C", caesar.getCipher());
+  private String encryptText(String text, int shift) {
+    Caesar caesar = new Caesar(text, shift);
+    return caesar.getCipher();
   }
 
   @Test
-  public void charAShift2ShouldBeD() {
-    Caesar caesar = new Caesar("A", 3);
-    assertEquals("D", caesar.getCipher());
+  public void TestShiftTextOneCharactor(){
+    assertEquals("B", encryptText("A", 1));
+    assertEquals("C", encryptText("A", 2));
+    assertEquals("D", encryptText("A", 3));
   }
 
   @Test
