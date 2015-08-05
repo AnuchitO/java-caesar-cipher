d1aea9c73330bde31f43bff6a23dbf55eb9a477b
Pead Nong <anuchit.prasertsang@gmail.com>
2015-08-06 00:10:03 +0700

    refactor to more than one shift

--- CaesarTest.java
+++ CaesarTest.java
@@ -8,33 +8,17 @@ public class CaesarTest {
   }
 
   @Test
-  public void TestShiftTextOneCharactor(){
+  public void TestEncryptTextOneCharactor(){
     assertEquals("B", encryptText("A", 1));
     assertEquals("C", encryptText("A", 2));
     assertEquals("D", encryptText("A", 3));
   }
 
   @Test
-  public void charAAShift1ShouldBeBB() {
-    Caesar caesar = new Caesar("AA", 1);
-    assertEquals("BB", caesar.getCipher());
-  }
-
-  @Test
-  public void charAAAShift1ShouldBeBBB() {
-    Caesar caesar = new Caesar("AAA", 1);
-    assertEquals("BBB", caesar.getCipher());
-  }
-
-  @Test
-  public void charAAAAShift2ShouldBeCCCC() {
-    Caesar caesar = new Caesar("AAAA", 2);
-    assertEquals("CCCC", caesar.getCipher());
-  }
-
-  @Test
-  public void charABCDEShift1ShouldBeBCDEF() {
-    Caesar caesar = new Caesar("ABCDE", 1);
-    assertEquals("BCDEF", caesar.getCipher());
+  public void TestEncryptTextMoreThanOneCharactor() {
+    assertEquals("BB", encryptText("AA", 1));
+    assertEquals("BBB", encryptText("AAA", 1));
+    assertEquals("CCCC", encryptText("AAAA", 2));
+    assertEquals("BCDEF", encryptText("ABCDE", 1));
   }
 }
