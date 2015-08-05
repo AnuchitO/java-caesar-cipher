ae8ede7807a2feef3f8cba213bc98b252cfcc4db
Pead Nong <anuchit.prasertsang@gmail.com>
2015-08-05 23:41:17 +0700

    refactor to for loop

--- Caesar.java
+++ Caesar.java
@@ -10,25 +10,11 @@ public class Caesar {
 
    public String getCipher() {
      byte[] byteText = this.text.getBytes();
-      
-     byteText[0] = (byte)(byteText[0]+this.shift);        
-
-     if(byteText.length == 2) {
-        byteText[1] = (byte)(byteText[1]+this.shift);        
-     }
-
-     if(byteText.length == 3) {
-        byteText[1] = (byte)(byteText[1]+this.shift);        
-        byteText[2] = (byte)(byteText[2]+this.shift);        
-     }
-
-     if(byteText.length == 4) {
-        byteText[1] = (byte)(byteText[1]+this.shift);        
-        byteText[2] = (byte)(byteText[2]+this.shift);        
-        byteText[3] = (byte)(byteText[3]+this.shift);        
-     }
+     
+     for (int index = 0;index < byteText.length; index++){
+          byteText[index] = (byte)(byteText[index]+this.shift);        
+     } 
 
      return new String(byteText);
-
    }
 }
